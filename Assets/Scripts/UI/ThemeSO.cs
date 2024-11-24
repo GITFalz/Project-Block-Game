90ad5d39a4dcd996c2b8110f707f82e7b8ca6f12
GITFalz <153421988+GITFalz@users.noreply.github.com>
2024-11-24 12:27:52 +0100

    feat : prefabs

---
+++ Assets/Scripts/UI/ThemeSO.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "ICustomUI/ThemeSO")]
+public class ThemeSO : ScriptableObject
+{
+    
+}
